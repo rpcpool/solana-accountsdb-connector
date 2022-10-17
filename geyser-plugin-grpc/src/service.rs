e1e45ac3e6354239b21bd13aefad86960c9aa855
Kirill Fomichev <fanatid@ya.ru>
2022-10-17 11:56:00 -0300

    remove extra channels

--- geyser-plugin-grpc/src/service.rs
+++ geyser-plugin-grpc/src/service.rs
@@ -1,17 +1,21 @@
 use {
     crate::accounts_selector::{AccountsSelector, AccountsSelectorConfig},
+    futures::stream::{self, Stream, StreamExt, TryStreamExt},
     log::*,
     proto::{
         accounts_db_server::AccountsDb, update::UpdateOneof, SubscribeRequest, SubscribeResponse,
         Update, UpdateAccountsSelectorRequest, UpdateAccountsSelectorResponse,
     },
     serde::Deserialize,
-    std::sync::{
-        atomic::{AtomicU64, Ordering},
-        Arc,
+    std::{
+        pin::Pin,
+        sync::{
+            atomic::{AtomicU64, Ordering},
+            Arc,
+        },
     },
     tokio::sync::{broadcast, mpsc},
-    tokio_stream::wrappers::ReceiverStream,
+    tokio_stream::wrappers::BroadcastStream,
     tonic::{Code, Request, Response, Result as TonicResult, Status},
 };
 
@@ -21,13 +25,12 @@ pub mod proto {
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct ServiceConfig {
-    broadcast_buffer_size: usize,
-    subscriber_buffer_size: usize,
+    #[serde(alias = "broadcast_buffer_size")]
+    channel_capacity: usize,
 }
 
 #[derive(Debug)]
 pub struct Service {
-    pub config: ServiceConfig,
     pub subscribe_id: AtomicU64,
     pub highest_write_slot: Arc<AtomicU64>,
     pub updates_tx: broadcast::Sender<Update>,
@@ -40,9 +43,8 @@ impl Service {
         highest_write_slot: Arc<AtomicU64>,
         accounts_selector_tx: mpsc::UnboundedSender<AccountsSelector>,
     ) -> Self {
-        let (updates_tx, _) = broadcast::channel(config.broadcast_buffer_size);
+        let (updates_tx, _) = broadcast::channel(config.channel_capacity);
         Self {
-            config,
             subscribe_id: AtomicU64::new(0),
             highest_write_slot,
             updates_tx,
@@ -53,7 +55,7 @@ impl Service {
 
 #[tonic::async_trait]
 impl AccountsDb for Service {
-    type SubscribeStream = ReceiverStream<TonicResult<Update>>;
+    type SubscribeStream = Pin<Box<dyn Stream<Item = TonicResult<Update>> + Send + 'static>>;
 
     async fn subscribe(
         &self,
@@ -62,37 +64,24 @@ impl AccountsDb for Service {
         let id = self.subscribe_id.fetch_add(1, Ordering::SeqCst);
         info!("{}, new subscriber", id);
 
-        let mut updates_rx = self.updates_tx.subscribe();
-
-        let (tx, rx) = mpsc::channel(self.config.subscriber_buffer_size);
-        let _ = tx.try_send(Ok(Update {
+        let response = Ok(Update {
             update_oneof: Some(UpdateOneof::SubscribeResponse(SubscribeResponse {
                 highest_write_slot: self.highest_write_slot.load(Ordering::Relaxed),
             })),
-        }));
+        });
 
-        tokio::spawn(async move {
-            let mut exit = false;
-            while !exit {
-                let fwd = updates_rx.recv().await.map_err(|error| {
-                    // Note: If we can't keep up pulling from the broadcast
-                    // channel here, there'll be a Lagged error, and we'll
-                    // close the connection because data was lost.
+        Ok(Response::new(
+            stream::once(async move { response })
+                .chain(BroadcastStream::new(self.updates_tx.subscribe()))
+                .map_err(move |error| {
                     warn!(
                         "{}, error while receiving message to be broadcast: {:?}",
                         id, error
                     );
-                    exit = true;
                     Status::new(Code::Internal, error.to_string())
-                });
-                if let Err(_err) = tx.send(fwd).await {
-                    info!("{}, subscriber stream closed", id);
-                    exit = true;
-                }
-            }
-        });
-
-        Ok(Response::new(ReceiverStream::new(rx)))
+                })
+                .boxed(),
+        ))
     }
 
     async fn update_accounts_selector(
